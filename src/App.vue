62190c341f40135b45ccf71fb7dacb549ae9e294
timeroute <chinesejar@outlook.com>
2022-12-18 21:03:27 +0800

    Init

---
+++ src/App.vue
@@ -0,0 +1,98 @@
+<script setup>
+import { ref } from 'vue'
+
+const theme = ref('light')
+const drawer = ref(true)
+const items = ref([
+  {
+    title: 'Foo',
+    value: 'foo',
+  },
+  {
+    title: 'Bar',
+    value: 'bar',
+  },
+  {
+    title: 'Fizz',
+    value: 'fizz',
+  },
+  {
+    title: 'Buzz',
+    value: 'buzz',
+  },
+])
+
+function onClick () {
+  theme.value = theme.value === 'light' ? 'dark' : 'light'
+}
+</script>
+
+<template>
+  <v-app :theme="theme">
+    <v-app-bar color="teal-darken-4" image="https://picsum.photos/1920/1080?random">
+       <template v-slot:image>
+        <v-img
+          gradient="to top right, rgba(19,84,122,.8), rgba(128,208,199,.8)"
+        ></v-img>
+      </template>
+      <v-toolbar-title>My files</v-toolbar-title>
+      <v-spacer></v-spacer>
+      <v-btn
+        :prepend-icon="theme === 'light' ? 'mdi-weather-sunny' : 'mdi-weather-night'"
+        @click="onClick"
+      >
+        Toggle
+      </v-btn>
+    </v-app-bar>
+    <v-navigation-drawer
+      v-model="drawer"
+      location="left"
+      permanent
+    >
+      <v-list
+        :items="items"
+      />
+    </v-navigation-drawer>
+    <v-main>
+      <v-container fluid>
+        <v-carousel>
+          <v-carousel-item
+            src="https://cdn.vuetifyjs.com/images/cards/docks.jpg"
+            cover
+          ></v-carousel-item>
+          <v-carousel-item
+            src="https://cdn.vuetifyjs.com/images/cards/hotel.jpg"
+            cover
+          ></v-carousel-item>
+          <v-carousel-item
+            src="https://cdn.vuetifyjs.com/images/cards/sunshine.jpg"
+            cover
+          ></v-carousel-item>
+        </v-carousel>
+         <v-text-field
+          v-model="name"
+          :counter="10"
+          :rules="nameRules"
+          label="Name"
+          required
+        ></v-text-field>
+        <v-row dense>
+          <v-col
+            v-for="n in 4"
+            :key="n"
+            cols="12"
+          >
+            <v-card
+              :title="`Content ${n}`"
+              :subtitle="`Subtitle for Content ${n}`"
+              text="Lorem ipsum dolor sit amet consectetur, adipisicing elit.?"
+            ></v-card>
+          </v-col>
+        </v-row>
+      </v-container>
+    </v-main>
+  </v-app>
+</template>
+
+<style>
+</style>
---
+++ src/main.js
@@ -0,0 +1,26 @@
+import { createApp } from "vue";
+import App from "./App.vue";
+import './assets/style.css';
+import 'vuetify/styles'
+import { createVuetify } from 'vuetify'
+import * as components from 'vuetify/components'
+import * as directives from 'vuetify/directives'
+import { aliases, mdi } from 'vuetify/iconsets/mdi'
+import '@mdi/font/css/materialdesignicons.css' // Ensure you are using css-loader
+
+const vuetify = createVuetify({
+  components,
+  directives,
+  icons: {
+    defaultSet: 'mdi',
+    aliases,
+    sets: {
+      mdi,
+    }
+  },
+})
+
+const app = createApp(App);
+app.use(vuetify);
+
+app.mount("#app");
